cbdc81538dc695191c34905a0488d90f1713a50e
Martin Dimitrov <dimitrov_martin@web.de>
2021-08-26 18:19:26 +0200

    fadeIn, out and To

---
+++ jQuery-cource/base/js/script.js
@@ -0,0 +1,8 @@
+$(function() {
+  // jQuery goes here...
+
+  // Uncomment this line to fade out the red box on page load
+  $('.red-box').fadeTo(1000, 0.2);
+  $('.green-box').fadeTo(1000, 0.5);
+  $('.blue-box').fadeTo(1000, 0.8);
+});
