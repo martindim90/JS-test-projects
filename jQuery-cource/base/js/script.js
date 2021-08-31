747e9182e2904f6d52ca8316612762ac9474dc6e
Martin Dimitrov <dimitrov_martin@web.de>
2021-08-31 18:20:26 +0200

    animations section finished

--- jQuery-cource/base/js/script.js
+++ jQuery-cource/base/js/script.js
@@ -2,7 +2,38 @@ $(function() {
   // jQuery goes here...
 
   // Uncomment this line to fade out the red box on page load
-  $('.red-box').fadeTo(1000, 0.2);
-  $('.green-box').fadeTo(1000, 0.5);
-  $('.blue-box').fadeTo(1000, 0.8);
+  // $('.red-box').fadeTo(1000, 0.2);
+  // $('.green-box').fadeTo(1000, 0.5);
+  // $('.blue-box').slideToggle(2000);
+
+  // $('.green-box').fadeToggle(1000);
+  // $('.green-box').fadeToggle(1000);
+
+  // $('p').hide();
+  // $('p').slideDown(2000);
+
+  // $('.blue-box').animate({
+  //   'margin-left': '+=200px'
+  // }, 1000, 'linear');
+
+  // $('.blue-box').animate({
+  //   'margin-left': '-=200px',
+  //   'border' : '5px solid yellow'
+  // }, 1000, 'linear');
+
+  // $('.blue-box').animate({
+  //   marginLeft: '+=200px',
+  //   opacity: '0',
+  //   height: '50px',
+  //   width: '50px',
+  //   marginTop: '25px'
+  // }, 4000);
+
+  // $('p').animate({
+  //   fontSize: '20px'
+  // }, 2000);
+
+  // $('.lightbox').delay(500).fadeIn(1000)
+
+  
 });
